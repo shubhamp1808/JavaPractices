1aa76710c82c426fed04bf80add1f6d50be9e8fd
Shubham Pandey <shubhampandey1808@gmail.com>
2023-12-14 01:23:44 +0530

    if array is sorted

--- DSA Practice/src/Arrays/IfArraySorted.java
+++ DSA Practice/src/Arrays/IfArraySorted.java
@@ -2,6 +2,19 @@ package Arrays;
 
 public class IfArraySorted {
     public static void main(String[] args) {
-        
+        int[] sample ={2,3,6,119,232,847};
+        int n = sample.length;
+        System.out.println("Is array sorted: " + findIfSorted(sample, n));
+    }
+
+    public static boolean findIfSorted(int[] a, int n){
+        if(n==0 || n==1)
+        return true;
+
+        for(int i=1; i<n; i++){
+            if(a[i-1]>a[i])
+            return false;
+        }
+        return true;
     }
 }
